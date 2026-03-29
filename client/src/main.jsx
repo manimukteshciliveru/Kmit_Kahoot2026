88277a93414b705164889e376e417e73d2858156
manimukteshciliveru_1218 <manimukteshciliveru@gmail.com>
2026-03-29 11:24:53 +0530

    Fix: resolve 'useTheme outside context' runtime error by adding ThemeProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,18 +4,21 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
 import { SocketProvider } from './context/SocketContext'
+import { ThemeProvider } from './context/ThemeContext'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <AuthProvider>
-        <SocketProvider>
-          <App />
-          <Toaster position="top-right" />
-        </SocketProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <SocketProvider>
+            <App />
+            <Toaster position="top-right" />
+          </SocketProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 )
